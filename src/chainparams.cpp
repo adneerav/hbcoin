dcba00ce22ac4a329e756c4d71431b2b40f1ca26
nirav.chauhan <nirav.chauhan@hiddenbrains.in>
2018-02-03 14:55:55 +0530

    change in checkpoint data

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -317,7 +317,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             {
-                {0, uint256S("530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9")},
+                {0, uint256S("5066625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0")},
             }
         };
 
